59a206e28c72a8f65d05d3d5cf5cee841ed90da1
Nathan Corvino <nathan@corvino.com>
2022-02-15 14:18:29 -0800

    Add selecting features on click with geojson  outline and feature list.

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ export default () => {
 
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(-121.3971);
+  const [lat, setLat] = useState(47.3896);
+  const [zoom, setZoom] = useState(15);
+  const [selectedFeatures, setSelectedFeatures] = useState([]);
 
   const tooltip = useRef(null);
   const tooltipContainer = useRef(null);
@@ -41,6 +42,22 @@ export default () => {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom
+    });
+
+    map.current.on("load", () => {
+      map.current.addSource("selectedFeatures", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: []
+        }
+      });
+
+      map.current.addLayer({
+        id: "selectedFeatures",
+        type: "line",
+        source: "selectedFeatures"
+      });
     });
 
     tooltipContainer.current = document.createElement("div");
@@ -72,17 +89,44 @@ export default () => {
       setTooltip(features);
     });
 
+    map.current.on("mouseout", () => {
+      setTooltip([]);
+    });
+
     map.current.on("mousedown", (e) => {
-      const features = map.current.queryRenderedFeatures(e.point);
-      console.log(features);
+      setSelectedFeatures(map.current.queryRenderedFeatures(e.point));
     });
   });
 
+  useEffect(() => {
+    if (!map.current || !map.current.loaded()) return;
+    const source = map.current.getSource("selectedFeatures");
+    source.setData({
+      type: "FeatureCollection",
+      features: selectedFeatures
+    });
+  }, [selectedFeatures]);
+
   return (
     <div>
-      <div className="absolute top-0 left-0 z-10 py-1 px-3 m-3 font-normal text-white bg-[#23374be5] rounded-md">
-        longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      <div className="absolute top-0 left-0 z-10">
+        <div className="py-1 px-3 m-3 font-normal text-white bg-[#23374be5] rounded-md">
+          longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+
+        {0 < selectedFeatures.length &&
+          <div className="py-1 px-3 m-3 font-normal text-white bg-[#23374be5] rounded-md">
+            <p className="float-right text-lg" onClick={ () => setSelectedFeatures([]) }>x</p>
+            <p className="text-lg font-bold">Features:</p>
+            <ul className="ml-2">
+              {selectedFeatures.map((feature, i) =>
+                (<li key={i}>{feature.layer["source-layer"]}: {feature.layer.id}</li>)
+              )}
+            </ul>
+          </div>
+        }
       </div>
+
       <div ref={mapContainer} className="w-full h-screen" />
     </div>
   );
